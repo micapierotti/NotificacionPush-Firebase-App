9ea1b05476b05ab83655b0237eb5a152785f3a86
micapierotti <micapierotti@live.com.ar>
2020-12-16 23:48:31 -0300

    Initial commit

---
+++ app/src/main/java/com/mpierotti/notificacionpush_firebaseapp/SplashScreen.java
@@ -0,0 +1,13 @@
+package com.mpierotti.notificacionpush_firebaseapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+    }
+}
